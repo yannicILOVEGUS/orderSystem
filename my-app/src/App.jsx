12c9b514a53c07c055aac5724e2e7cc7e87bdd5b
Philipp Huber <philipp.huber@gus-group.com>
2025-01-08 14:48:03 +0100

    commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import Header from './Components/Header/Header'
+import Products from './Components/products/products'
 
 function App() {
   return(
     <div>
       <Header/>
+      <Products/>
     </div>
   )
   
---
+++ my-app/src/Components/products/products.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './products.css'
+import '../styles/containers.css'
+
+const products = () => {
+    return(
+       <div className="homepageContainer">
+            <h1>TEST</h1>
+       </div>
+    )
+}
+
+export default products
