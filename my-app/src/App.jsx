70a504c0c70b4431cd04dfe55f2ad85a27018827
Yannic Peter <yannic.peter@gus-group.com>
2025-01-08 16:25:13 +0100

    footer

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Header from './Components/Header/Header'
 import Products from './Components/products/products'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return(
     <div>
       <Header/>
       <Products/>
+      <Footer/>
     </div>
   )
   
