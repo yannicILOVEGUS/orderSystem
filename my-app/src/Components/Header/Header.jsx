fad3ff1f143585017cd4451e24cab0779c538b55
Yannic Peter <yannic.peter@gus-group.com>
2025-01-08 13:39:40 +0100

    0.1

---
+++ my-app/src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.css'
+
+const Header = () => {
+    return(
+       <header className="header">
+         <img src="Logo.png"></img>
+       </header>
+    )
+}
+
+export default Header
