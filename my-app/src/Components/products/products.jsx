b1b199d0aafe5c1ac555f77c0ec2035e2745e34f
Yannic Peter <yannic.peter@gus-group.com>
2025-01-09 15:34:15 +0100

    fixBug

--- my-app/src/Components/products/products.jsx
+++ my-app/src/Components/products/products.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './products.css';
 import '../styles/containers.css';
 import { useCart } from '../Context/CartStoreContext';
