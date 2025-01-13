c8c8ef8144e681bd2043e6b31f5772edd63493ea
Yannic Peter <yannic.peter@gus-group.com>
2025-01-13 14:57:13 +0100

    Desgin Summery

--- my-app/src/Components/Checkout/OrderSummary.jsx
+++ my-app/src/Components/Checkout/OrderSummary.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Link, useLocation  } from 'react-router-dom';
-
+import { Link, useLocation } from 'react-router-dom';
+import './OrderSummary.css';
 
 const OrderSummary = () => {
   const location = useLocation();
   const { orderNumber, warenkorb, gesamtPreis } = location.state || {};
+
   return (
-    <div style={{ textAlign: 'center', padding: '20px' }}>
+    <div className='OrderSummaryContainer'>
       <h2>Thank you for your purchase!</h2>
-      <p>Your order number is: {orderNumber}</p>
+      <p>Your order number is: <span className='highlight'>{orderNumber}</span></p>
       <h3>Your Items:</h3>
+      <ul className='itemList'>
+        {warenkorb && warenkorb.map((item, index) => (
+          <li key={index} className='item'>
+            <span>{item.name}</span> <span>{item.menge}</span> <span>{item.preis * item.menge} €</span>
+          </li>
+        ))}
+      </ul>
+      <p className='totalPrice'>Total Price: <span className='highlight'>{gesamtPreis} €</span></p>
       <Link to="/">
-        <button>Back to Homepage</button>
+        <button className='backButton'>Back to Homepage</button>
       </Link>
     </div>
   );
